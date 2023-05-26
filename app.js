163d5f701934400462927142deb7a2e2e5578b68
rijasm09@gmail.com <rijasm09@gmil.com>
2023-05-26 16:00:36 +0530

    created routes and views

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
 const app = express();
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use('/add-product',(req,res)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
-})
-app.post('/product',(req,res,next,)=>{
-    console.log(req.body);
-    res.redirect('/')
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+app.use((req, res, next) => {
+    res.status(404).send('<h1>oops...Page Not Found</h1>')
 })
 
-app.use('/',(req,res,next)=>{
-    res.send('<h1>welcome to expressjs</h1>')
-})
 
-app.listen(3000, ()=> {
+app.listen(3000, () => {
     console.log("server listening on port 3000");
 });
 
---
+++ routes/admin.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+
+const router = express.Router();
+
+//   /admin/add-product => GET
+router.get('/add-product', (req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+})
+
+//   /admin/add-product => POST
+router.post('/add-product', (req, res, next,) => {
+    console.log(req.body);
+    res.redirect('/')
+})
+
+module.exports = router;
+
